a16b51fdd0f5ad9d06d05a07f5d516509dce4aea
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-07-14 20:07:34 +0200

    Project skeleton
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ya_jsonapi",
+    version='0.0.1',
+    description="Yet another jsonapi client library. Sync and async, with (TODO) optional orm and caching",
+    long_description="TODO",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: BSD License",
+    ],
+    author="Igor Kotrasiński",
+    author_email="i.kotrasinsk@gmail.com",
+    url="https://github.com/Wesmania/ya-jsonapi",
+    keywords="JSONAPI JSON API client",
+    license="BSD-3",
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "requests",
+        "aiohttp",
+    ],
+)
